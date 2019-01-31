3Qax/Skarbnik

commit 8fdf0a79962fc6d9b9956a5cc0c0e39e4c71e3d5

Implemented reminder feature. Unfortunately introduced a bug that cause PaidCells to not show correctly after cell expansion.

skarbnik/Controller/PaymentViewController.swift
@@ -15,8 +15,6 @@ class PaymentViewController: UIViewController {
     let eventStore = EKEventStore()
     let classPickViewController = ClassPickViewController()
     
-    
-    
     override func loadView() {
         view = PaymentView(frame: UIScreen.main.bounds)
     }
@@ -34,12 +32,14 @@ class PaymentViewController: UIViewController {
     }
 }
 
+//MARK:
 extension PaymentViewController: PaymentViewProtocol {
     func didTappedClass() {
         self.present(classPickViewController, animated: false)
     }
 }
 
+//MARK:
 extension PaymentViewController: PickerProtocol {
     func didChoose(_ index: Int, completion: @escaping () -> ()) {
         
@@ -55,10 +55,14 @@ extension PaymentViewController: PickerProtocol {
     }
 }
 
+//MARK:
 extension PaymentViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("section: \(indexPath.section) row: \(indexPath.row)")
+    }
 }
 
+//MARK:
 extension PaymentViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
@@ -101,45 +105,6 @@ extension PaymentViewController: UITableViewDataSource {
             fatalError("TableView was ask for cell for unexpected section with number: \(indexPath.section)")
         }
         
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("section: \(indexPath.section) row: \(indexPath.row)")
-    }
-}
-
-extension PaymentViewController: PendingPaymentCellProtocool {
-    func didTappedRemindButton(sender: PendingPaymentCellView) {
-        
-        switch EKEventStore.authorizationStatus(for: .reminder) {
-        case .notDetermined:
-            eventStore.requestAccess(to: .reminder) { (succed, error: Error?) in
-                guard succed else { return }
-                self.didTappedRemindButton(sender: sender)
-            }
-        case .authorized:
-            let reminder = EKReminder(eventStore: eventStore)
-            reminder.title = "Zapłacić za \(paymentModel!.pendingPayments[sender.index!].name)"
-            let alarm = EKAlarm(absoluteDate: Calendar.current.date(byAdding: .day, value: -1, to: paymentModel!.pendingPayments[sender.index!].end_date)!)
-            reminder.addAlarm(alarm)
-            let calendars = eventStore.calendars(for: .reminder)
-            reminder.calendar = calendars.first
-            do {
-                try eventStore.save(reminder, commit: true)
-            } catch {
-                print(error.localizedDescription)
-            }
-        case .restricted:
-            print("restricted")
-        case .denied:
-            print("denied")
-        }
-    }
-}
-
-extension PaymentViewController: PaidPaymentCellProtocool {
-    func didTapped(sender: PaidPaymentCellView) {
-        sender.toggle()
     }
     
 }

skarbnik/View/for cell/PaidPaymentCellView.swift
@@ -12,6 +12,7 @@ import SnapKit
 
 class PaidPaymentCellView: PaymentCell {
     var delegate: PaidPaymentCellProtocool?
+    var contentViewBottomConstraint: Constraint?
     private var isExpanded = false
     
     var showPhotosButton: IconButton = {
@@ -34,10 +35,6 @@ class PaidPaymentCellView: PaymentCell {
     }
     
     func setup(_ title: String, _ description: String, _ amount: Float) {
-        let tapGestureRecoginzer = UITapGestureRecognizer(target: self, action: #selector(gotTapped(sender:)))
-        contentView.isUserInteractionEnabled = true
-        contentView.addGestureRecognizer(tapGestureRecoginzer)
-
         setupBasicViews(withContent: {
             self.amountLabel.text = String.localizedStringWithFormat("%.2f%@", amount, "zł")
             self.amountLabel.textColor = Color.grey.base
@@ -48,31 +45,45 @@ class PaidPaymentCellView: PaymentCell {
         self.amountLabel.snp.makeConstraints({ (make) in
             make.bottom.equalToSuperview().offset(-5)
         })
+        
+        tapFunc = toggle
     }
     
     func toggle() {
         if isExpanded {
+            
+            showPhotosButton.removeFromSuperview()
+            descriptionLabel.removeFromSuperview()
+            
+            self.amountLabel.snp.remakeConstraints({ (make) in
+                make.top.equalToSuperview().offset(5)
+                make.right.equalToSuperview().offset(-self.separatorInset.left)
+                make.bottom.equalToSuperview().offset(-5)
+            })
             
             isExpanded = false
         } else {
             contentView.addSubview(showPhotosButton)
             contentView.addSubview(descriptionLabel)
             amountLabel.snp.remakeConstraints { (make) in
-                make.top.equalToSuperview().offset(5)
+                make.top.equalTo(titleLabel).priority(.required)
                 make.right.equalToSuperview().offset(-self.separatorInset.left)
+                make.left.equalTo(titleLabel.snp.right).offset(10)
             }
+            self.descriptionLabel.setContentCompressionResistancePriority(.required, for: .vertical)
             descriptionLabel.snp.remakeConstraints { (make) in
-                make.top.equalTo(titleLabel.snp.bottom)
-                make.left.equalToSuperview().offset(20)
-                make.right.equalTo(self.amountLabel)
+                make.top.equalTo(titleLabel.snp.bottom).priority(.required).labeled("Top of description to the bottom of title")
+                make.left.equalToSuperview().offset(20).labeled("Left offset of description")
+                make.right.equalTo(self.amountLabel).labeled("Right of description equal to amountLabel")
             }
+            self.showPhotosButton.setContentCompressionResistancePriority(.required, for: .vertical)
             showPhotosButton.snp.makeConstraints { (make) in
-                make.top.equalTo(descriptionLabel.snp.bottom).offset(5)
+                make.top.equalTo(descriptionLabel.snp.bottom).offset(5).priority(.required).labeled("Top of showPhotosButton to description bottom")
                 make.left.right.equalTo(descriptionLabel)
-                make.height.equalTo(30)
-                make.bottom.equalToSuperview().offset(-5)
+                make.height.equalTo(30).priority(999)
+                make.bottom.equalTo(contentView).priority(1000)
             }
-            contentView.sizeToFit()
+            
             isExpanded = true
         }
     }

skarbnik/View/for cell/PaymentCell.swift
@@ -13,13 +13,13 @@ class PaymentCell: UITableViewCell {
     
     var titleLabel: UILabel! = {
     let label = UILabel()
-    label.numberOfLines = 0 //odpowiada za zawijanie tekstu w obrębie UILabel
+    label.numberOfLines = 0
     label.font = UIFont(name: "HelveticaNeue", size: 22.0)
     return label
-}()
+    }()
     var descriptionLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 0 //odpowiada za zawijanie tekstu w obrębie UILabel
+        label.numberOfLines = 0
         label.font = UIFont(name: "HelveticaNeue-Light", size: 18.0)
         label.textColor = UIColor(rgb: 0x78C0E5)
         return label
@@ -27,20 +27,33 @@ class PaymentCell: UITableViewCell {
     var amountLabel: UILabel! = {
         let label = UILabel()
         label.textAlignment = .right
-        label.numberOfLines = 0 //odpowiada za zawijanie tekstu w obrębie UILabel
         label.font = UIFont(name: "HelveticaNeue-Light", size: 22.0)
         label.textColor = UIColor(rgb: 0xFA3CB1)
         return label
     }()
+    var tapFunc: (() -> ())?
+    
+    @objc func handleTap() {
+        if let tapFunc = tapFunc {
+            tapFunc()
+        } else {
+            print("Tapped")
+        }
+    }
 
     func setupBasicViews(withContent: () -> ()) {
+        
+        let tapGestureRecoginzer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        contentView.isUserInteractionEnabled = true
+        contentView.addGestureRecognizer(tapGestureRecoginzer)
+        
         contentView.addSubview(titleLabel)
         contentView.addSubview(amountLabel)
         
         //Title
         self.titleLabel.setContentCompressionResistancePriority(.required, for: .vertical)
         titleLabel.snp.makeConstraints { (make) in
-            make.top.equalToSuperview().offset(5)
+            make.top.equalToSuperview().offset(5).priority(.required)
             make.left.equalToSuperview().offset(20)
         }
         
@@ -48,7 +61,7 @@ class PaymentCell: UITableViewCell {
         self.amountLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
         self.amountLabel.setContentHuggingPriority(.required, for: .horizontal)
         amountLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(titleLabel)
+            make.top.equalTo(titleLabel).priority(.required)
             make.right.equalToSuperview().offset(-self.separatorInset.left)
             make.left.equalTo(titleLabel.snp.right).offset(10)
         }

skarbnik.xcodeproj/project.pbxproj
@@ -22,6 +22,8 @@
 		197F583821D049750098F17B /* PickerProtocol.swift in Sources */ = {isa = PBXBuildFile; fileRef = 197F583721D049750098F17B /* PickerProtocol.swift */; };
 		198E731D21ED4AC600053994 /* PaidPaymentCellProtocool.swift in Sources */ = {isa = PBXBuildFile; fileRef = 198E731C21ED4AC600053994 /* PaidPaymentCellProtocool.swift */; };
 		19A4ADEB21E62AF100CC2E42 /* PaymentCell.swift in Sources */ = {isa = PBXBuildFile; fileRef = 1943528021E6259900F84F41 /* PaymentCell.swift */; };
+		19A7403321EEB30B009BC735 /* PaymentViewPaidPaymentCellController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 19A7403221EEB30B009BC735 /* PaymentViewPaidPaymentCellController.swift */; };
+		19A7403521EEB32C009BC735 /* PaymentViewPendingPaymentCellController.swift in Sources */ = {isa = PBXBuildFile; fileRef = 19A7403421EEB32C009BC735 /* PaymentViewPendingPaymentCellController.swift */; };
 		19CA3D5B21C9B530009059B0 /* LoginView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 19CA3D5A21C9B530009059B0 /* LoginView.swift */; };
 		19CA3D5F21CAE272009059B0 /* UserModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 19CA3D5E21CAE272009059B0 /* UserModel.swift */; };
 		19D7065A21AFC38E00AE2546 /* PaymentView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 19D7065921AFC38E00AE2546 /* PaymentView.swift */; };
@@ -49,6 +51,8 @@
 		197F583521D042CA0098F17B /* ClassPickViewProtocol.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ClassPickViewProtocol.swift; sourceTree = "<group>"; };
 		197F583721D049750098F17B /* PickerProtocol.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PickerProtocol.swift; sourceTree = "<group>"; };
 		198E731C21ED4AC600053994 /* PaidPaymentCellProtocool.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PaidPaymentCellProtocool.swift; sourceTree = "<group>"; };
+		19A7403221EEB30B009BC735 /* PaymentViewPaidPaymentCellController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PaymentViewPaidPaymentCellController.swift; sourceTree = "<group>"; };
+		19A7403421EEB32C009BC735 /* PaymentViewPendingPaymentCellController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PaymentViewPendingPaymentCellController.swift; sourceTree = "<group>"; };
 		19CA3D5A21C9B530009059B0 /* LoginView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = LoginView.swift; sourceTree = "<group>"; };
 		19CA3D5E21CAE272009059B0 /* UserModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = UserModel.swift; sourceTree = "<group>"; };
 		19D7065921AFC38E00AE2546 /* PaymentView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PaymentView.swift; sourceTree = "<group>"; };
@@ -128,6 +132,8 @@
 			children = (
 				1941528D21A96A8300D0A7A3 /* LoginViewController.swift */,
 				19FC745F21A9CAAD0019F1A5 /* PaymentViewController.swift */,
+				19A7403221EEB30B009BC735 /* PaymentViewPaidPaymentCellController.swift */,
+				19A7403421EEB32C009BC735 /* PaymentViewPendingPaymentCellController.swift */,
 				19549A0121CD3284009C994C /* ClassPickViewController.swift */,
 			);
 			path = Controller;
@@ -310,7 +316,9 @@
 				19FC745E21A9922A0019F1A5 /* loginViewProtocol.swift in Sources */,
 				19CA3D5B21C9B530009059B0 /* LoginView.swift in Sources */,
 				19A4ADEB21E62AF100CC2E42 /* PaymentCell.swift in Sources */,
+				19A7403521EEB32C009BC735 /* PaymentViewPendingPaymentCellController.swift in Sources */,
 				1943527C21E546A800F84F41 /* PaidPaymentCellView.swift in Sources */,
+				19A7403321EEB30B009BC735 /* PaymentViewPaidPaymentCellController.swift in Sources */,
 				19642BD321EC9E2D00ECA28C /* PendingPaymentCellProtocool.swift in Sources */,
 				1904C22F21A9F99300477024 /* PaymentModel.swift in Sources */,
 				197F583821D049750098F17B /* PickerProtocol.swift in Sources */,

skarbnik/Controller/PaymentViewPaidPaymentCellController.swift
@@ -1,0 +1,17 @@
+//
+//  PaymentViewPaidPaymentCellController.swift
+//  skarbnik
+//
+//  Created by Jakub Towarek on 16/01/2019.
+//  Copyright © 2019 Jakub Towarek. All rights reserved.
+//
+
+import Foundation
+
+//MARK: 
+extension PaymentViewController: PaidPaymentCellProtocool {
+    func didTapped(sender: PaidPaymentCellView) {
+        sender.toggle()
+    }
+    
+}

skarbnik/Controller/PaymentViewPendingPaymentCellController.swift
@@ -1,0 +1,39 @@
+//
+//  PaymentViewPendingPaymentCellController.swift
+//  skarbnik
+//
+//  Created by Jakub Towarek on 16/01/2019.
+//  Copyright © 2019 Jakub Towarek. All rights reserved.
+//
+
+import EventKit
+
+//MARK:
+extension PaymentViewController: PendingPaymentCellProtocool {
+    func didTappedRemindButton(sender: PendingPaymentCellView) {
+        
+        switch EKEventStore.authorizationStatus(for: .reminder) {
+        case .notDetermined:
+            eventStore.requestAccess(to: .reminder) { (succed, error: Error?) in
+                guard succed else { return }
+                self.didTappedRemindButton(sender: sender)
+            }
+        case .authorized:
+            let reminder = EKReminder(eventStore: eventStore)
+            reminder.title = "Zapłacić za \(paymentModel!.pendingPayments[sender.index!].name)"
+            let alarm = EKAlarm(absoluteDate: Calendar.current.date(byAdding: .day, value: -1, to: paymentModel!.pendingPayments[sender.index!].end_date)!)
+            reminder.addAlarm(alarm)
+            let calendars = eventStore.calendars(for: .reminder)
+            reminder.calendar = calendars.first
+            do {
+                try eventStore.save(reminder, commit: true)
+            } catch {
+                print(error.localizedDescription)
+            }
+        case .restricted:
+            print("restricted")
+        case .denied:
+            print("denied")
+        }
+    }
+}

skarbnik/View/PaymentView.swift
@@ -64,8 +64,8 @@ class PaymentView: UIView {
             make.top.equalTo(headerView.snp.bottom)
         }
 
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 150
+//        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
         
         tableView.register(PendingPaymentCellView.self, forCellReuseIdentifier: "PendingCell")
         tableView.register(PaidPaymentCellView.self, forCellReuseIdentifier: "PaidCell")

commit e310c03cfdb20b77b865256cb721241b264a45c6

Fixed bug introduced in 8fdf0a79962fc6d9b9956a5cc0c0e39e4c71e3d5 - now cells expand nicely.

skarbnik/Controller/PaymentViewController.swift
@@ -106,6 +106,7 @@ extension PaymentViewController: UITableViewDataSource {
             let data = paymentModel!.paidPayments[indexPath.row]
             cell.setup(data.name, data.description, data.amount)
             cell.delegate = self
+            cell.tableView = (self.view as! PaymentView).tableView
             return cell
         default:
             fatalError("TableView was ask for cell for unexpected section with number: \(indexPath.section)")

skarbnik/Controlls/LightTextField.swift
@@ -25,8 +25,11 @@ class LightTextField: UITextField {
         
         if let image = image {
             self.leftViewMode = .always
-            let imageView = UIImageView(image: image.tint(with: UIColor(rgb: 0xAAAAAA)))
+            //let imageView = UIImageView(image: image.tint(with: UIColor(rgb: 0xAAAAAA)))
+            let imageView = UIImageView(image: image)
             imageView.contentMode = .scaleAspectFit
+            //TODO: make sure that line below works
+            imageView.tintColor = UIColor(rgb: 0xAAAAAA)
             let customLeftView = UIView(frame: CGRect(x: 0, y: 0, width: imageView.frame.width + padding, height: imageView.frame.height))
             
             customLeftView.addSubview(imageView)

skarbnik/Controlls/OptionButton.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class OptionButton: UIButton {
 
-    init(title: String = "Button", hight: CGFloat = 40.0) {
+    init(title: String = "Button", height: CGFloat = 40.0) {
         super.init(frame: .zero)
         
         setTitle(title.capitalizingFirstLetter(), for: .normal)
@@ -19,12 +19,12 @@ class OptionButton: UIButton {
         
         backgroundColor = UIColor.clear//(rgb: 0xFA3CB1)
         
-        layer.cornerRadius = hight / 2.0
+        layer.cornerRadius = height / 2.0
         layer.borderColor = UIColor(rgb: 0xFA3CB1).cgColor
         layer.borderWidth = 1.0
         
         snp.makeConstraints { (make) in
-            make.height.equalTo(hight)
+            make.height.equalTo(height)
         }
     }
     

skarbnik/View/for cell/PaidPaymentCellView.swift
@@ -7,28 +7,17 @@
 //
 
 import UIKit
-import Material
 import SnapKit
 
 class PaidPaymentCellView: PaymentCell {
+    //reference to tableView so that cell could notify
+    //it when performing updates
+    weak var tableView: UITableView?
     var delegate: PaidPaymentCellProtocool?
-    var contentViewBottomConstraint: Constraint?
-    private var isExpanded = false
+    let showPhotosButton = OptionButton(title: "Pokaż zdjęcia", height: 30)
     
-    var showPhotosButton: IconButton = {
-        let btn = IconButton(title: "POKAŻ ZDJĘCIA", titleColor: UIColor.init(rgb: 0xFA3CB1))
-        
-        btn.pulseColor = UIColor.init(rgb: 0xFA3CB1)
-        btn.backgroundColor = Color.clear
-        btn.borderColor = UIColor.init(rgb: 0xFA3CB1)
-        
-        btn.borderWidthPreset = .border2
-        btn.cornerRadiusPreset = .cornerRadius4
-        
-        btn.titleLabel?.font = UIFont(name: "PingFangTC-Light", size: 18.0)
-        
-        return btn
-    }()
+    
+    private var isExpanded = false
     
     @objc func gotTapped(sender: Any) {
         delegate?.didTapped(sender: self)
@@ -51,7 +40,7 @@ class PaidPaymentCellView: PaymentCell {
     
     func toggle() {
         if isExpanded {
-            
+            tableView?.beginUpdates()
             showPhotosButton.removeFromSuperview()
             descriptionLabel.removeFromSuperview()
             
@@ -60,9 +49,10 @@ class PaidPaymentCellView: PaymentCell {
                 make.right.equalToSuperview().offset(-self.separatorInset.left)
                 make.bottom.equalToSuperview().offset(-5)
             })
-            
+            tableView?.endUpdates()
             isExpanded = false
         } else {
+            tableView?.beginUpdates()
             contentView.addSubview(showPhotosButton)
             contentView.addSubview(descriptionLabel)
             amountLabel.snp.remakeConstraints { (make) in
@@ -80,12 +70,12 @@ class PaidPaymentCellView: PaymentCell {
             showPhotosButton.snp.makeConstraints { (make) in
                 make.top.equalTo(descriptionLabel.snp.bottom).offset(5).priority(.required).labeled("Top of showPhotosButton to description bottom")
                 make.left.right.equalTo(descriptionLabel)
-                make.height.equalTo(30).priority(999)
-                make.bottom.equalTo(contentView).priority(1000)
+                make.bottom.equalToSuperview().offset(-5).priority(999)
             }
-            
+            tableView?.endUpdates()
             isExpanded = true
         }
+        //TODO: make sure that newly expanded cell is fully shown
     }
     
 }

skarbnik/View/for cell/PaymentCell.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2018 Jakub Towarek. All rights reserved.
 //
 import UIKit
-import Material
 import SnapKit
 
 class PaymentCell: UITableViewCell {
@@ -60,6 +59,7 @@ class PaymentCell: UITableViewCell {
         //Amount
         self.amountLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
         self.amountLabel.setContentHuggingPriority(.required, for: .horizontal)
+        self.amountLabel.setContentHuggingPriority(.required, for: .vertical)
         amountLabel.snp.makeConstraints { (make) in
             make.top.equalTo(titleLabel).priority(.required)
             make.right.equalToSuperview().offset(-self.separatorInset.left)

skarbnik/View/for cell/PendingPaymentCellView.swift
@@ -7,12 +7,11 @@
 //
 
 import UIKit
-import Material
 
 class PendingPaymentCellView: PaymentCell {
     var index: Int?
     var delegate: PendingPaymentCellProtocool?
-    var remindButton = OptionButton(title: "Przypomnij", hight: 30.0)
+    var remindButton = OptionButton(title: "Przypomnij", height: 30.0)
     var payButton = RaisedButton(title: "Zapłać", hight: 30.0)
     
     @objc func remindButtonTapped(sender: Any) {