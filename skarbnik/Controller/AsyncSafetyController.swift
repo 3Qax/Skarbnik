3Qax/Skarbnik

commit 9f9f7a2f80d7d5372164be3d30191647926c9f91

End of ModelLayer refactor. Introduced a bug - pull to refresh and sometimes changing a student cause a crash "NSInternalInconsistencyException". Made timeout longer, because backend often need more time to respond.

skarbnik/Controller/PaymentViewController.swift
@@ -16,12 +16,8 @@ class PaymentViewController: UIViewController {
     
     
     
-    override func loadView() {
-        view = PaymentView(frame: UIScreen.main.bounds)
-    }
-    
-    init(of id: Int) {
-        paymentModel = PaymentModel(of: id)
+    init(of studentID: Int, in classID: Int) {
+        paymentModel = PaymentModel(of: studentID, in: classID)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -29,13 +25,19 @@ class PaymentViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func loadView() {
+        view = PaymentView(frame: UIScreen.main.bounds)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //don't allow user to change student while loading one
-        (self.view as! PaymentView).headerChangeStudentImageView.isUserInteractionEnabled = false
-        (self.view as! PaymentView).headerChangeStudentImageView.alpha = 0.3
+//        (self.view as! PaymentView).headerChangeStudentImageView.isUserInteractionEnabled = false
+//        (self.view as! PaymentView).headerChangeStudentImageView.alpha = 0.3
         
+        NotificationCenter.default.addObserver(self, selector: #selector(updatePendingPaymentsSection), name: .modelChangedPendingPayemnts, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updatePaidPaymentsSection), name: .modelChangedPaidPayemnts, object: nil)
         
         selectionFeedbackGenerator.prepare()
         
@@ -45,6 +47,17 @@ class PaymentViewController: UIViewController {
         (self.view as! PaymentView).tableView.delegate = self
     }
     
+    @objc func updatePendingPaymentsSection() {
+        DispatchQueue.main.async {
+            (self.view as! PaymentView).tableView.reloadSections(IndexSet(integer: 0), with: .right)
+        }
+    }
+    @objc func updatePaidPaymentsSection() {
+        DispatchQueue.main.async {
+            (self.view as! PaymentView).tableView.reloadSections(IndexSet(integer: 1), with: .right)
+        }
+    }
+    
 }
 
 extension PaymentViewController: PaymentViewProtocol {
@@ -52,15 +65,11 @@ extension PaymentViewController: PaymentViewProtocol {
     //provide student changing functionality
     func didTappedClass() {
         selectionFeedbackGenerator.prepare()
-        coordinator?.didLoginSuccessfully(passwordChangeRequired: false)
+        coordinator?.didLoginSuccessfully()
     }
     
-    //refresh data for selectedChild
-    func refreshData(completion: @escaping () -> ()) {
-        paymentModel.refreshData(completion: {
-            (self.view as! PaymentView).tableView.reloadData()
-            completion()
-        })
+    func didRequestDataRefresh(completion: () -> ()) {
+        paymentModel.refreshData()
     }
 }
 
@@ -84,16 +93,15 @@ extension PaymentViewController: UITableViewDataSource {
         switch indexPath.section {
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "PendingCell") as! PendingPaymentCellView
-            let data = paymentModel.pendingPayments[indexPath.row]
-            cell.setup(data.name, data.description, data.amount)
+            let dataKey = paymentModel.pendingPayments.index(paymentModel.pendingPayments.startIndex, offsetBy: indexPath.row)
+            cell.setup(paymentModel.pendingPayments[dataKey].value.name, paymentModel.pendingPayments[dataKey].value.description, paymentModel.pendingPayments[dataKey].value.amount)
             cell.delegate = self
-            cell.index = indexPath.row
+            cell.key = paymentModel.pendingPayments[dataKey].key
             return cell
         case 1:
             let cell = tableView.dequeueReusableCell(withIdentifier: "PaidCell") as! PaidPaymentCellView
-            print(indexPath.row)
-            let data = paymentModel.paidPayments[indexPath.row]
-            cell.setup(data.name, data.description, data.amount)
+            let dataKey = paymentModel.paidPayments.index(paymentModel.paidPayments.startIndex, offsetBy: indexPath.row)
+            cell.setup(paymentModel.paidPayments[dataKey].value.name, paymentModel.paidPayments[dataKey].value.description, paymentModel.paidPayments[dataKey].value.amount)
             cell.delegate = self
             cell.tableView = (self.view as! PaymentView).tableView
             return cell

skarbnik/MainCoordinator.swift
@@ -23,18 +23,26 @@ class MainCoordinator {
         navigationController.pushViewController(loginVC, animated: false)
     }
     
-    func didLoginSuccessfully(passwordChangeRequired: Bool) {
-        if passwordChangeRequired {
-            //TODO: create modal pop-up telling that user must change a password
-            let ChangePasswordVC = ChangePasswordViewController()
-            navigationController.pushViewController(ChangePasswordVC, animated: true)
-            return
-        }
+    func didLoginSuccessfully() {
+        asyncPasswordController()
         let pickStudentVC = PickStudentViewController()
         pickStudentVC.modalPresentationStyle = .overCurrentContext
         pickStudentVC.coordinator = self
         navigationController.present(pickStudentVC, animated: true)
     }
+    
+    func asyncPasswordController() {
+        func passwordChangeRequired() {
+            let ChangePasswordVC = ChangePasswordViewController()
+            navigationController.pushViewController(ChangePasswordVC, animated: true)
+            return
+        }
+        
+        //TODO: create modal pop-up telling that user must change a password
+//        passwordChangeRequired()
+    }
+    
+
     
     func didRequestStudentChange() {
         let pickStudentVC = PickStudentViewController()
@@ -42,8 +50,8 @@ class MainCoordinator {
         navigationController.pushViewController(pickStudentVC, animated: false)
     }
     
-    func didChooseStudent(with id: Int) {
-        let paymentVC = PaymentViewController(of: id)
+    func didChooseStudent(of studentID: Int, in classID: Int) {
+        let paymentVC = PaymentViewController(of: studentID, in: classID)
         paymentVC.coordinator = self
         navigationController.pushViewController(paymentVC, animated: true)
     }

skarbnik/Model/APIClient.swift
@@ -10,8 +10,8 @@ import Foundation
 
 class APIClient {
     
-    private let baseURL: URLComponents = URLComponents(string: "https://quiet-caverns-69534.herokuapp.com:443")!
-    //private let baseURL: URLComponents = URLComponents(string: "http://localhost:8000")!
+    //private let baseURL: URLComponents = URLComponents(string: "https://quiet-caverns-69534.herokuapp.com:443")!
+    private let baseURL: URLComponents = URLComponents(string: "http://localhost:8000")!
     private var Token: String {
         guard UserDefaults.standard.string(forKey: "JWT") != nil else {
             fatalError("Token not found!")
@@ -47,7 +47,7 @@ class APIClient {
         return customizedURL.url!
     }
     
-    private func createRequest(_ method: RequestMethod, from url: URL, addingValueHeader headers: [String : String]? = nil, addingData data: Data? = nil, timeout: TimeInterval = 10, authorise: Bool = true) -> URLRequest {
+    private func createRequest(_ method: RequestMethod, from url: URL, addingValueHeader headers: [String : String]? = nil, addingData data: Data? = nil, timeout: TimeInterval = 20, authorise: Bool = true) -> URLRequest {
         
         var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: timeout)
         

commit caa4332b94c3d58ec2e4a8bc94fea018c4d62b25

Fixed bug introduced in 9f9f7a2f80d7d5372164be3d30191647926c9f91.Added improvements in mainCoordinator. Fixed AsyncSafetyController which was cause of few retain cycles.

skarbnik/Controller/AsyncSafetyController.swift
@@ -11,28 +11,65 @@ import UIKit
 
 
 class AsyncSafetyController {
-    let model = AsyncSafetyModel()
-    var coordinator: MainCoordinator?
+    private let model = AsyncSafetyModel()
+    weak var coordinator: MainCoordinator?
+    
+    private var lastLoginWasUnsuccessful: Bool?
+    private var dateOfUnsuccessfulLogin: String?
+    private var ipOfUnsuccessfulLogin: String?
+    private var doRequirePasswordChange: Bool?
+    
+    var shouldShow: Bool = false {
+        didSet {
+            if shouldShow, let doRequirePasswordChange = doRequirePasswordChange, let lastLoginWasUnsuccessful = lastLoginWasUnsuccessful {
+                if doRequirePasswordChange {
+                    DispatchQueue.main.async {
+                        self.coordinator?.shouldChangePassword()
+                    }
+                }
+                if lastLoginWasUnsuccessful {
+                    DispatchQueue.main.async {
+                        self.coordinator?.shouldWarnAboutLastLogin(on: self.dateOfUnsuccessfulLogin ?? "", from: self.ipOfUnsuccessfulLogin ?? "")
+                    }
+                }
+                coordinator?.asyncSafetyController = nil
+            }
+        }
+    }
+
     
     init() {
         model.delegate = self
     }
+    
+    deinit {
+        print("AsyncSafetyController dealocated!")
+    }
 }
 
 extension AsyncSafetyController: AsyncSafetyProtocool {
+    
+    func dontRequirePasswordChange() {
+        doRequirePasswordChange = false
+    }
+    
+    func lastLoginSuccessful() {
+        lastLoginWasUnsuccessful = false
+    }
+    
     func requirePasswordChange() {
-        DispatchQueue.main.async {
-            self.coordinator?.shouldChangePassword()
-        }
+        print("require password change")
+        doRequirePasswordChange = true
     }
     
     func lastLoginUnsuccessful(_ date: Date, fromIP ip: String) {
+        print("last login unsuccessful")
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         formatter.timeStyle = .medium
-        DispatchQueue.main.async {
-            self.coordinator?.shouldWarnAboutLastLogin(on: formatter.string(from: date), from: ip)
-        }
+        dateOfUnsuccessfulLogin = formatter.string(from: date)
+        ipOfUnsuccessfulLogin = ip
+        lastLoginWasUnsuccessful = true
     }
     
 }

skarbnik/Controller/LastLoginAttemptFailedAlertControler.swift
@@ -26,11 +26,11 @@ class LastLoginAttemptFailedAlertControler: UIViewController {
                                                             preferredStyle: .alert)
         super.init(nibName: nil, bundle: nil)
         self.lastLoginUnsuccessfulAlert.addAction(UIAlertAction(title: NSLocalizedString("last_login_failed_change_password_button_text", comment: ""), style: .default, handler: { _ in
-            self.dismiss(animated: true)
             self.coordinator?.didRequestPasswordChange()
+            self.coordinator?.navigationController.dismiss(animated: true)
         }))
         self.lastLoginUnsuccessfulAlert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: { _ in
-            self.dismiss(animated: true)
+            self.coordinator?.navigationController.dismiss(animated: true)
         }))
     }
     

skarbnik/Controller/PaymentViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import EventKit
 
 class PaymentViewController: UIViewController {
-    var paymentModel: PaymentModel
+    let paymentModel: PaymentModel
     let eventStore      = EKEventStore()
     var coordinator: MainCoordinator?
     
@@ -46,28 +46,27 @@ class PaymentViewController: UIViewController {
     
     @objc func updatePendingPaymentsSection(notification: Notification) {
         DispatchQueue.main.sync {
-            print("notification - pending")
             (self.view as! PaymentView).tableView.beginUpdates()
-            print((self.view as! PaymentView).tableView.numberOfRows(inSection: 0))
             (self.view as! PaymentView).tableView.insertRows(at: [IndexPath(row: (self.view as! PaymentView).tableView.numberOfRows(inSection: 0), section: 0)], with: .right)
             (self.view as! PaymentView).tableView.endUpdates()
         }
     }
     @objc func updatePaidPaymentsSection() {
         DispatchQueue.main.sync {
-            print("notification - paid")
             (self.view as! PaymentView).tableView.beginUpdates()
-            print((self.view as! PaymentView).tableView.numberOfRows(inSection: 1))
             (self.view as! PaymentView).tableView.insertRows(at: [IndexPath(row: (self.view as! PaymentView).tableView.numberOfRows(inSection: 1), section: 1)], with: .right)
             (self.view as! PaymentView).tableView.endUpdates()
         }
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        coordinator?.shouldStartAsyncSafetyController()
+        coordinator?.asyncSafetyController?.shouldShow = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        
+        NotificationCenter.default.removeObserver(self, name: .modelChangedPendingPayemnts, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .modelChangedPaidPayemnts, object: nil)
         
         (self.view as! PaymentView).tableView.beginUpdates()
         
@@ -95,22 +94,23 @@ extension PaymentViewController: PaymentViewProtocol {
         coordinator?.didRequestStudentChange()
     }
     
-    func didRequestDataRefresh(completion: () -> ()) {
+    func didRequestDataRefresh(completion: @escaping () -> ()) {
         (self.view as! PaymentView).tableView.beginUpdates()
         
         for i in 0..<(self.view as! PaymentView).tableView.numberOfRows(inSection: 0) {
             (self.view as! PaymentView).tableView.deleteRows(at: [IndexPath(row: i, section: 0)], with: .right)
         }
-        paymentModel.pendingPayments.removeAll(keepingCapacity: true)
         
         for i in 0..<(self.view as! PaymentView).tableView.numberOfRows(inSection: 1) {
             (self.view as! PaymentView).tableView.deleteRows(at: [IndexPath(row: i, section: 1)], with: .right)
         }
-        paymentModel.paidPayments.removeAll(keepingCapacity: true)
         
-        (self.view as! PaymentView).tableView.endUpdates()
-        
-        paymentModel.refreshData()
+        paymentModel.refreshData(deletedDataHandler: {
+            (self.view as! PaymentView).tableView.endUpdates()
+        }, completion: {
+            completion()
+        })
+
     }
 }
 
@@ -133,14 +133,12 @@ extension PaymentViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.section {
         case 0:
-            print("requested pending cell for \(indexPath.row)")
             let cell = tableView.dequeueReusableCell(withIdentifier: "PendingCell") as! PendingPaymentCellView
             cell.setup(paymentModel.pendingPayments[indexPath.row]!.name, paymentModel.pendingPayments[indexPath.row]!.description, paymentModel.pendingPayments[indexPath.row]!.amount)
             cell.delegate = self
             cell.key = indexPath.row
             return cell
         case 1:
-            print("requested paid cell for \(indexPath.row)")
             let cell = tableView.dequeueReusableCell(withIdentifier: "PaidCell") as! PaidPaymentCellView
             cell.setup(paymentModel.paidPayments[indexPath.row]!.name, paymentModel.paidPayments[indexPath.row]!.description, paymentModel.paidPayments[indexPath.row]!.amount)
 

skarbnik/Helper/AsyncSafetyProtocool.swift
@@ -8,7 +8,9 @@
 
 import Foundation
 
-protocol AsyncSafetyProtocool {
+protocol AsyncSafetyProtocool: class {
     func requirePasswordChange()
     func lastLoginUnsuccessful(_ date: Date, fromIP ip: String)
+    func dontRequirePasswordChange()
+    func lastLoginSuccessful()
 }

skarbnik/MainCoordinator.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class MainCoordinator {
     var navigationController: UINavigationController
+    var asyncSafetyController: AsyncSafetyController?
     
     
     init(navigationController: UINavigationController){
@@ -27,31 +28,24 @@ class MainCoordinator {
         let pickStudentVC = PickStudentAlertController()
         pickStudentVC.coordinator = self
         pickStudentVC.modalPresentationStyle = .overCurrentContext
-        navigationController.pushViewController(pickStudentVC, animated: true)
-//        navigationController.present(pickStudentVC, animated: true)
+        navigationController.present(pickStudentVC, animated: true)
+    
+        asyncSafetyController = AsyncSafetyController()
+        asyncSafetyController!.coordinator = self
     }
     
     func didRequestStudentChange() {
         let pickStudentVC = PickStudentAlertController()
         pickStudentVC.coordinator = self
         pickStudentVC.modalPresentationStyle = .overCurrentContext
-        navigationController.pushViewController(pickStudentVC, animated: true)
-//        navigationController.present(pickStudentVC, animated: true)
+        navigationController.present(pickStudentVC, animated: true)
     }
     
     func didChooseStudent(of studentID: Int, in classID: Int) {
-        navigationController.popViewController(animated: true)
-        
-        let paymentVC = PaymentViewController(of: studentID, in: classID)
-        paymentVC.coordinator = self
-        navigationController.pushViewController(paymentVC, animated: true)
-//        navigationController.present(paymentVC, animated: true)
-    }
-    
-    func shouldStartAsyncSafetyController() {
-        DispatchQueue.global(qos: .background).async {
-            let asyncSafetyController = AsyncSafetyController()
-            asyncSafetyController.coordinator = self
+        navigationController.dismiss(animated: true) {
+            let paymentVC = PaymentViewController(of: studentID, in: classID)
+            paymentVC.coordinator = self
+            self.navigationController.pushViewController(paymentVC, animated: true)
         }
     }
     

skarbnik/Model/APIClient.swift
@@ -10,8 +10,9 @@ import Foundation
 
 class APIClient {
     
-    private let baseURL: URLComponents = URLComponents(string: "https://quiet-caverns-69534.herokuapp.com:443")!
-    //private let baseURL: URLComponents = URLComponents(string: "http://localhost:8000")!
+    //private let baseURL: URLComponents = URLComponents(string: "https://quiet-caverns-69534.herokuapp.com:443")!
+    private let baseURL: URLComponents = URLComponents(string: "http://172.22.0.47:8000")!
+    
     private var Token: String {
         guard UserDefaults.standard.string(forKey: "JWT") != nil else {
             fatalError("Token not found!")

skarbnik/Model/AsyncSafetyModel.swift
@@ -12,7 +12,7 @@ import Foundation
 
 class AsyncSafetyModel {
     let apiClient = APIClient()
-    var delegate: AsyncSafetyProtocool?
+    weak var delegate: AsyncSafetyProtocool?
     private var activities: [Activity] = [Activity]()
     
     private func encode<T: Encodable>(_ data: T) -> Data {
@@ -75,9 +75,15 @@ class AsyncSafetyModel {
         })
     }
     
+    deinit {
+        print("AsyncSafetyModel dealocated!")
+    }
+    
     func interpretActivities() {
         if activities.count == 1 {
             delegate?.requirePasswordChange()
+        } else {
+            delegate?.dontRequirePasswordChange()
         }
         
         for i in 0..<activities.count {
@@ -85,6 +91,8 @@ class AsyncSafetyModel {
                 let longDateFormatter = ISO8601DateFormatter()
                 let date = longDateFormatter.date(from: activities[1].login_datetime)!
                 delegate?.lastLoginUnsuccessful(date, fromIP: activities[1].login_IP)
+            } else {
+                delegate?.lastLoginSuccessful()
             }
         }
     }

skarbnik/Model/PaymentModel.swift
@@ -9,20 +9,22 @@
 import Foundation
 
 class PaymentModel {
-    private let apiClient       = APIClient()
+    private let apiClient           = APIClient()
+    private let dispatchGroup       = DispatchGroup()
+    private var onRefreshCompletion: () -> () = { print("refreshing ended") }
     private let classID: Int
     private let studentID: Int
-    public  var pendingPayments  = [Int: Payment]() {
+    public  var pendingPayments     = [Int: Payment]() {
         didSet {
             if pendingPayments.count > 0 { NotificationCenter.default.post(name: .modelChangedPendingPayemnts, object: self) }
         }
     }
-    public  var paidPayments     = [Int: Payment]() {
+    public  var paidPayments        = [Int: Payment]() {
         didSet {
             if paidPayments.count > 0 { NotificationCenter.default.post(name: .modelChangedPaidPayemnts, object: self) }
         }
     }
-    private var recivedPayments  = [Payment]()
+    private var recivedPayments     = [Payment]()
     
     struct PaymentPacket: Codable {
         let id_field: Int
@@ -33,7 +35,7 @@ class PaymentModel {
     }
     
     class Payment {
-        let paymentModel: PaymentModel
+        private weak var paymentModel: PaymentModel?
         let id_field: Int
         let name, description: String
         let amount: Float
@@ -77,9 +79,9 @@ class PaymentModel {
                 let amount_paid: String
             }
             
-            paymentModel.apiClient.request(.paymentDetail,
+            paymentModel!.apiClient.request(.paymentDetail,
                                queryItems: [URLQueryItem(name: "payment", value: String(id_field)),
-                                            URLQueryItem(name: "student", value: String(paymentModel.studentID))],
+                                            URLQueryItem(name: "student", value: String(paymentModel!.studentID))],
                                completion: { (succeed, data) in
                                 guard succeed else {
                                     print("Getting contributions of Payment: \(self.id_field) failed!")
@@ -102,12 +104,11 @@ class PaymentModel {
         func classify() {
             let sum = contribution?.reduce(0, +) ?? 0
             if sum  == amount {
-                paymentModel.paidPayments[paymentModel.paidPayments.count] = self
-                //paymentModel.paidPayments[id_field] = self
+                paymentModel!.paidPayments[paymentModel!.paidPayments.count] = self
             } else {
-                paymentModel.pendingPayments[paymentModel.pendingPayments.count] = self
-                //paymentModel.pendingPayments[id_field] = self
+                paymentModel!.pendingPayments[paymentModel!.pendingPayments.count] = self
             }
+            paymentModel!.dispatchGroup.leave()
         }
     }
     
@@ -120,16 +121,22 @@ class PaymentModel {
     }
     
     init(of studentID:Int, in classID: Int) {
-        print("of student\(studentID) and class id \(classID)")
         self.studentID = studentID
         self.classID = classID
         fetchPayments()
     }
     
-    func refreshData() {
-//        pendingPayments.removeAll()
-//        paidPayments.removeAll()
+    func refreshData(deletedDataHandler: () -> (), completion: @escaping () -> ()) {
+        
+        pendingPayments.removeAll(keepingCapacity: true)
+        paidPayments.removeAll(keepingCapacity: true)
+        recivedPayments.removeAll(keepingCapacity: true)
+        deletedDataHandler()
+        
+        self.onRefreshCompletion = completion
+        
         fetchPayments()
+        
     }
     
     func fetchPayments() {
@@ -144,8 +151,14 @@ class PaymentModel {
                             //This is required due to Date Type handeling dinffrence between Backend and Swift
                             let recivedPaymentsPackets = self.decode([PaymentPacket].self, from: data!)
                             for paymentPacket in recivedPaymentsPackets {
+                                self.dispatchGroup.enter()
                                 self.recivedPayments.append(Payment(data: paymentPacket, paymentModel: self))
                             }
+                            
+                            self.dispatchGroup.notify(queue: .main) {
+                                self.onRefreshCompletion()
+                            }
+                            
                           }))
 
     }

skarbnik/View/PaymentView.swift
@@ -127,6 +127,7 @@ class PaymentView: UIView {
                 self.refreshControl.backgroundColor = UIColor(rgb: 0xFA3CB1)
             }
             self.refreshControl.endRefreshing()
+            notificationFeedbackGenerator.notificationOccurred(.success)
         })
         
     }